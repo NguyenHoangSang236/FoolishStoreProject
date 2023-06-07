daaacf7064cc061d72aadcc32064acc527ce32af
NguyenHoangSang236 <nguyenhoangsang236@gmail.com>
2023-06-07 17:47:51 +0700

    fix for deploy

--- src/main/java/com/backend/core/repository/InvoiceRepository.java
+++ src/main/java/com/backend/core/repository/InvoiceRepository.java
@@ -33,40 +33,40 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
 	
 //	@Query(value = "select * from invoice where id = :idVal", nativeQuery = true)
 //	Invoice getInvoiceById(@Param("idVal") int id);
-	
-	
-	@Query(value = "select * from invoice where Payment_Method != 'cod'", nativeQuery = true)
-	List<Invoice> getOnlinePaymentInvoices();
-	
-	
-	@Query(value = "select * from invoice where Payment_Method = 'cod' and admin_acceptance = 'waiting' and delivery_status <> 'not shipped'", nativeQuery = true)
-	List<Invoice> getWaitingCodInvoices();
-	
-	
-	@Query(value = "select * from invoice where delivery_status = 'packing'", nativeQuery = true)
-	List<Invoice> getPackingCodInvoices();
-	
-	
-	@Query(value = "select * from invoice i join delivery d on i.id = d.invoice_id where shipper_id = :idVal and delivery_status = 'shipping'", nativeQuery = true)
-	List<Invoice> getShipperInvoicesList(@Param("idVal") int id);
-	
-	
-	@Query(value = "select * from invoice i join delivery d on i.id = d.invoice_id where current_status = 'shipping'", nativeQuery = true)
-	List<Invoice> getShippingInvoicesList();
-	
-	
-	@Query(value = "select * from invoice i join delivery d on i.id = d.invoice_id where current_status = 'failed'", nativeQuery = true)
-	List<Invoice> getFailedInvoicesList();
-	
-	
-	@Query(value = "select * from invoice i join delivery d on i.id = d.invoice_id where current_status = 'success'", nativeQuery = true)
-	List<Invoice> getSuccessfulInvoicesList();
-	
-	
-	@Query(value = "select * from invoice where delivery_status = 'packing'", nativeQuery = true)
-	List<Invoice> getPackingInvoicesList();
-	
-	
-	@Query(value = "select * from invoice where delivery_status = 'not shipped'", nativeQuery = true)
-	List<Invoice> getCustomerCanceledInvoicesList();
+//
+//
+//	@Query(value = "select * from invoice where Payment_Method != 'cod'", nativeQuery = true)
+//	List<Invoice> getOnlinePaymentInvoices();
+//
+//
+//	@Query(value = "select * from invoice where Payment_Method = 'cod' and admin_acceptance = 'waiting' and delivery_status <> 'not shipped'", nativeQuery = true)
+//	List<Invoice> getWaitingCodInvoices();
+//
+//
+//	@Query(value = "select * from invoice where delivery_status = 'packing'", nativeQuery = true)
+//	List<Invoice> getPackingCodInvoices();
+//
+//
+//	@Query(value = "select * from invoice i join delivery d on i.id = d.invoice_id where shipper_id = :idVal and delivery_status = 'shipping'", nativeQuery = true)
+//	List<Invoice> getShipperInvoicesList(@Param("idVal") int id);
+//
+//
+//	@Query(value = "select * from invoice i join delivery d on i.id = d.invoice_id where current_status = 'shipping'", nativeQuery = true)
+//	List<Invoice> getShippingInvoicesList();
+//
+//
+//	@Query(value = "select * from invoice i join delivery d on i.id = d.invoice_id where current_status = 'failed'", nativeQuery = true)
+//	List<Invoice> getFailedInvoicesList();
+//
+//
+//	@Query(value = "select * from invoice i join delivery d on i.id = d.invoice_id where current_status = 'success'", nativeQuery = true)
+//	List<Invoice> getSuccessfulInvoicesList();
+//
+//
+//	@Query(value = "select * from invoice where delivery_status = 'packing'", nativeQuery = true)
+//	List<Invoice> getPackingInvoicesList();
+//
+//
+//	@Query(value = "select * from invoice where delivery_status = 'not shipped'", nativeQuery = true)
+//	List<Invoice> getCustomerCanceledInvoicesList();
 }
