24da4860e35d75890690a486c28d230aa86e336d
NguyenHoangSang236 <nguyenhoangsang236@gmail.com>
2023-06-07 17:09:09 +0700

    fix for deploy

--- src/main/java/com/backend/core/entity/tableentity/Comment.java
+++ src/main/java/com/backend/core/entity/tableentity/Comment.java
@@ -58,46 +58,4 @@ public class Comment {
         this.content = content;
         this.commentDate = commentDate;
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Date getCommentDate() {
-        return commentDate;
-    }
-
-    public void setCommentDate(Date commentDate) {
-        this.commentDate = commentDate;
-    }
-
-
 }
