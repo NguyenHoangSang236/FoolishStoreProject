62cad821e57376d42b849a6d4717b0394482c99c
NguyenHoangSang236 <nguyenhoangsang236@gmail.com>
2023-06-07 21:00:12 +0700

    fix for deploy

--- src/main/java/com/backend/core/repository/ProductManagementRepository.java
+++ src/main/java/com/backend/core/repository/ProductManagementRepository.java
@@ -9,10 +9,10 @@ import com.backend.core.entity.tableentity.ProductManagement;
 
 @Repository
 public interface ProductManagementRepository extends JpaRepository<ProductManagement, Integer>{
-	@Query(value = "select * from products_management where product_id = :idVal order by id desc limit 1", nativeQuery = true)
-	ProductManagement getLastestProductManagementInfoByProductId(@Param("idVal") int productId);
-	
-	
+//	@Query(value = "select * from products_management where product_id = :idVal order by id desc limit 1", nativeQuery = true)
+//	ProductManagement getLastestProductManagementInfoByProductId(@Param("idVal") int productId);
+//
+//
 //	@Query(value = "select id from products_management order by id desc limit 1", nativeQuery = true)
 //	int getLastProductManagementId();
 
