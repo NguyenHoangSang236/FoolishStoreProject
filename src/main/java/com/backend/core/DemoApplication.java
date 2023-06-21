4424176da8ec0c050a20f33a483298fe39d00de5
NguyenHoangSang236 <nguyenhoangsang236@gmail.com>
2023-06-22 00:34:54 +0700

    update main and source handler

--- src/main/java/com/backend/core/DemoApplication.java
+++ src/main/java/com/backend/core/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.backend.core;
 
+import com.backend.core.util.SourceCodeHandlerUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.io.IOException;
 import java.util.Collections;
 
 
@@ -16,7 +18,9 @@ import java.util.Collections;
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class} )
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		SourceCodeHandlerUtil.deleteDerectory("/home/mr/JAVA/FoolishStoreProject/tokens");
+
 		SpringApplication app = new SpringApplication(DemoApplication.class);
 		app.setDefaultProperties(Collections.singletonMap("server.port", "8080"));
 		app.run(args);
