8c8b2b6b7e92ce94f64fedbd9d791d55481749e2
Cao Đinh Sỹ Vỹ <caodinhsyvy@Vys-Mac.local>
2023-12-03 17:12:08 +0000

    remove build.sh file

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-./mvnw clean install
-rm splited-app-* | true
-split -b 21M target/FoolishStoreProject-0.0.1-SNAPSHOT.jar splited-app-
-git add .
-git commit -m 'build'
-git push origin build
-ssh root@14.225.254.87 'cd FoolishStoreProject && git pull origin build && cat splited-app-* > FoolishStoreProject-0.0.1-SNAPSHOT.jar && docker compose up -d --build'
