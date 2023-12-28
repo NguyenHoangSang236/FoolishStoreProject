6ccb99eebd87b7776f18cfcc2b17615fe432a47d
sang.nguyen <sang.nguyen>
2023-12-28 16:32:54 +0700

    build

--- src/main/java/com/backend/core/entities/tableentity/Product.java
+++ src/main/java/com/backend/core/entities/tableentity/Product.java
@@ -16,9 +16,7 @@ import org.hibernate.annotations.OnDeleteAction;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 @Getter
