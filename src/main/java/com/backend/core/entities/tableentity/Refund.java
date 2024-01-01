7ab49ab1b3239fcdd47ba89c2e581901a51239ad
sang.nguyen <sang.nguyen>
2024-01-01 22:56:23 +0700

    build

--- src/main/java/com/backend/core/entities/tableentity/Refund.java
+++ src/main/java/com/backend/core/entities/tableentity/Refund.java
@@ -1,5 +1,7 @@
 package com.backend.core.entities.tableentity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -40,7 +42,13 @@ public class Refund {
     @JoinColumn(name = "in_charge_admin_id")
     Staff staff;
 
+    @JsonIgnore
     @OneToOne
     @JoinColumn(name = "invoice_id", referencedColumnName = "ID")
     Invoice invoice;
+
+    @JsonProperty("invoiceId")
+    public int invoiceID() {
+        return this.invoice.getId();
+    }
 }
