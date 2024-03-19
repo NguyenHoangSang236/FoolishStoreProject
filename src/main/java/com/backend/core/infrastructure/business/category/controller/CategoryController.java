eb8b994b6e6b5545ad1a94368cbbfdd96065b4de
NguyenHoangSang236 <nguyenhoangsang236@gmail.com>
2024-03-19 18:05:20 +0700

    fix cate controller at view all cate

--- src/main/java/com/backend/core/infrastructure/business/category/controller/CategoryController.java
+++ src/main/java/com/backend/core/infrastructure/business/category/controller/CategoryController.java
@@ -3,10 +3,18 @@ package com.backend.core.infrastructure.business.category.controller;
 import com.backend.core.entity.CrudController;
 import com.backend.core.entity.api.ApiResponse;
 import com.backend.core.entity.category.model.Catalog;
+import com.backend.core.infrastructure.config.api.ResponseMapper;
+import com.backend.core.usecase.UseCaseExecutorImpl;
 import com.backend.core.usecase.service.CrudService;
 import com.backend.core.usecase.statics.RenderTypeEnum;
+import com.backend.core.usecase.usecases.category.AddCategoryUseCase;
+import com.backend.core.usecase.usecases.category.DeleteCategoryUseCase;
+import com.backend.core.usecase.usecases.category.FilterCategoryUseCase;
+import com.backend.core.usecase.usecases.category.UpdateCategoryUseCase;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.hibernate.sql.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
@@ -15,60 +23,60 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping(consumes = {"*/*"}, produces = {MediaType.APPLICATION_JSON_VALUE})
-// @CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "true")
-public class CategoryController extends CrudController {
+@AllArgsConstructor
+public class CategoryController {
+    final UseCaseExecutorImpl useCaseExecutor;
+    final AddCategoryUseCase addCategoryUseCase;
+    final DeleteCategoryUseCase deleteCategoryUseCase;
+    final FilterCategoryUseCase filterCategoryUseCase;
+    final UpdateCategoryUseCase updateCategoryUseCase;
 
-    public CategoryController(@Autowired @Qualifier("CategoryCrudServiceImpl") CrudService categoryCrudServiceImpl) {
-        super(categoryCrudServiceImpl);
-        super.crudService = categoryCrudServiceImpl;
-    }
 
     @PostMapping("/authen/category/add")
     @PreAuthorize("hasAuthority('ADMIN')")
-    @Override
-    public ResponseEntity<ApiResponse> addNewItem(@RequestBody String json, HttpServletRequest httpRequest) throws IOException {
+    public CompletableFuture<ResponseEntity<ApiResponse>> addNewCategory(@RequestBody String json, HttpServletRequest httpRequest) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         Catalog catalog = objectMapper.readValue(json, Catalog.class);
-        return crudService.singleCreationalResponse(catalog, httpRequest);
+        return useCaseExecutor.execute(
+                addCategoryUseCase,
+                new AddCategoryUseCase.InputValue(catalog),
+                ResponseMapper::map
+        );
     }
 
     @PostMapping("/authen/category/update")
     @PreAuthorize("hasAuthority('ADMIN')")
-    @Override
-    public ResponseEntity<ApiResponse> updateItem(@RequestBody String json, HttpServletRequest httpRequest) throws IOException {
+    public CompletableFuture<ResponseEntity<ApiResponse>> updateCategory(@RequestBody String json, HttpServletRequest httpRequest) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         Catalog catalog = objectMapper.readValue(json, Catalog.class);
-        return crudService.updatingResponseByRequest(catalog, httpRequest);
+        return useCaseExecutor.execute(
+                updateCategoryUseCase,
+                new UpdateCategoryUseCase.InputValue(catalog),
+                ResponseMapper::map
+        );
     }
 
-    @Override
-    public ResponseEntity<ApiResponse> readSelectedItemById(int id, HttpServletRequest httpRequest) throws IOException {
-        return null;
-    }
 
     @GetMapping("authen/category/delete_category_id={id}")
     @PreAuthorize("hasAuthority('ADMIN')")
-    @Override
-    public ResponseEntity<ApiResponse> deleteSelectedItemById(@PathVariable int id, HttpServletRequest httpRequest) throws IOException {
-        return crudService.removingResponseById(id, httpRequest);
+    public CompletableFuture<ResponseEntity<ApiResponse>> deleteCategoryById(@PathVariable int id, HttpServletRequest httpRequest) throws IOException {
+        return useCaseExecutor.execute(
+                deleteCategoryUseCase,
+                new DeleteCategoryUseCase.InputValue(id),
+                ResponseMapper::map
+        );
     }
 
-    @Override
-    public ResponseEntity<ApiResponse> updateSelectedItemById(int id, HttpServletRequest httpRequest) throws IOException {
-        return null;
-    }
-
-    @Override
     @GetMapping("/unauthen/category/allCategories")
-    public ResponseEntity<ApiResponse> getListOfItems(String json, HttpServletRequest httpRequest) throws IOException {
-        return crudService.readingResponse(RenderTypeEnum.ALL_CATEGORIES.name(), httpRequest);
-    }
-
-    @Override
-    public ResponseEntity<ApiResponse> getListOfItemsFromFilter(String json, HttpServletRequest httpRequest) throws IOException {
-        return null;
+    public CompletableFuture<ResponseEntity<ApiResponse>> getListOfItems(String json, HttpServletRequest httpRequest) throws IOException {
+        return useCaseExecutor.execute(
+                deleteCategoryUseCase,
+                new DeleteCategoryUseCase.InputValue(id),
+                ResponseMapper::map
+        );
     }
 }
