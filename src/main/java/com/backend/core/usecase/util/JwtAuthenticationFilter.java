ef81518fea0cecd8a053665b06102696c119f1f7
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-04-03 23:49:13 +0700

    fix security

--- src/main/java/com/backend/core/usecase/util/JwtAuthenticationFilter.java
+++ src/main/java/com/backend/core/usecase/util/JwtAuthenticationFilter.java
@@ -20,7 +20,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.UUID;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -70,8 +69,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                         SecurityContextHolder.getContext().setAuthentication(authToken);
-
-                        System.out.println("-----Finish filter-------");
                     }
                 }
             }
