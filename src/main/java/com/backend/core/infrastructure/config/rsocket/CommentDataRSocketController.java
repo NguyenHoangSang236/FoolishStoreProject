f7d4b9ec3abc6bc3739155d077785d3220e4ef68
NguyenHoangSang236 <nguyenhoangsang236@gmail.com>
2024-04-16 18:09:25 +0700

    update

--- src/main/java/com/backend/core/infrastructure/config/rsocket/CommentDataRSocketController.java
+++ src/main/java/com/backend/core/infrastructure/config/rsocket/CommentDataRSocketController.java
@@ -1,25 +1,39 @@
 package com.backend.core.infrastructure.config.rsocket;
 
 import com.backend.core.entity.comment.model.Comment;
+import com.backend.core.infrastructure.business.comment.dto.CommentRenderInfoDTO;
+import com.backend.core.infrastructure.business.comment.repository.CommentRenderInfoRepository;
 import com.backend.core.infrastructure.business.comment.repository.CommentRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
 @Controller
 public class CommentDataRSocketController {
     @Autowired
-    CommentRepository commentRepository;
+    CommentRenderInfoRepository commentRenderInfoRepo;
+    @Autowired
+    CommentRepository commentRepo;
 
     @MessageMapping("currentCommentData")
-    public String currentCommentData(String commentId) {
-        return commentId;
+    public Map<String, Object> currentCommentData(String commentId) throws JsonProcessingException {
+        CommentRenderInfoDTO commentRenderInfo = commentRenderInfoRepo.getCommentById(Integer.parseInt(commentId));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map = objectMapper.convertValue(commentRenderInfo, new TypeReference<Map<String, Object>>() {});
+
+        return map;
     }
 
     @MessageMapping("addCommentData")
     public Mono<Void> collectMarketData(Comment comment) {
-        commentRepository.save(comment);
+        commentRepo.save(comment);
         return Mono.empty();
     }
 }
