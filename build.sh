c184132f21866f136b21f1708767f993b7fbad42
Cao Đinh Sỹ Vỹ <caodinhsyvy@Vys-Mac.local>
2024-06-20 16:39:25 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ split -b 21M target/FoolishStoreProject-0.0.1-SNAPSHOT.jar splited-app-
 git add .
 git commit -m 'build'
 git push origin build
-ssh root@103.200.20.153 'cd FoolishStoreProject && git pull origin build && cat splited-app-* > target/FoolishStoreProject-0.0.1-SNAPSHOT.jar && docker rm -f spring-boot-container && docker compose up -d --build && rm -r splited-app-*'
+ssh root@103.200.20.153 'cd FoolishStoreProject && git pull origin build && cat splited-app-* > target/FoolishStoreProject-0.0.1-SNAPSHOT.jar && docker rm -f spring-boot-container && docker compose up -d --build && rm -r splited-app-* && rm target/FoolishStoreProject-0.0.1-SNAPSHOT.jar'
